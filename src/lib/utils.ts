4cf05904456fb911c09eec4a0700de1b47bab79a
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2024-03-17 17:09:10 +0900

    fix by eslint, modify table of content and slug page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,11 @@
-export const pathJoin = (path: string, subPath: string) => {
-  return (
-    '/' +
+export const pathJoin = (path: string, subPath: string) => (
+  `/${
     path
       .split('/')
       .concat(subPath.split('/'))
       .filter((p) => p)
-      .join('/')
-  )
-}
+      .join('/')}`
+);
 
 const Months: string[] = [
   'Jan',
@@ -22,20 +20,20 @@ const Months: string[] = [
   'Oct',
   'Nov',
   'Dec',
-]
+];
 
 export const formatDate = (date: string) => {
   try {
-    const yyyy = date.slice(0, 4)
-    const mm = date.slice(5, 7)
-    const dd = date.slice(8, 10)
-    const y = new Number(yyyy)
-    const m = new Number(mm)
-    const d = new Number(dd)
+    const yyyy = date.slice(0, 4);
+    const mm = date.slice(5, 7);
+    const dd = date.slice(8, 10);
+    const y = new Number(yyyy);
+    const m = new Number(mm);
+    const d = new Number(dd);
 
-    return `${Months[(m as number) - 1]} ${d}, ${y}`
+    return `${Months[(m as number) - 1]} ${d}, ${y}`;
   } catch (e) {
-    console.error(e)
-    return '----'
+    console.error(e);
+    return '----';
   }
-}
+};
