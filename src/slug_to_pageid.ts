c1dc589033bb4ed37e4612ed0ff3ae7f7b530c0b
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2024-04-25 10:16:55 +0900

    add images

--- src/slug_to_pageid.ts
+++ src/slug_to_pageid.ts
@@ -7,4 +7,7 @@ export const SLUG_PAGE_ID_MAPPING: SlugPageIdMapping = {
   'ist-ci-2023S-programming': 'b47562e419164f849cb0fddb0113f865',
   'australia-visa-600': 'e23b0bd4bb004afdb1dcfbac9cfcab21',
   'harumi-flag-1month': '2dec4611e3374b1ebc4ee377dcb55c40',
+  'test-page': '96dda59c5bd3417c9e68d1e3849f1df0',
+  'supported-blocks': '5e49332da86d4dea99ad6d852f57a9c9',
+  'what-is-astro-notion-blog': 'c9a90f296aba485f84c247f0c7ded822',
 };
